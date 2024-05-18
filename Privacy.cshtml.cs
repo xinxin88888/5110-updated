3220cb84ac36bebf19bdd276c05eab40963a4990
Tim Harris <twolfeh@gmail.com>
2024-05-18 11:49:21 -0700

    Change products, design of website

---
+++ Privacy.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace ContosoCrafts.WebSite.Pages
+{
+    /// <summary>
+    /// PrivacyModel used for Privacy Page
+    /// </summary>
+    public class PrivacyModel : PageModel
+    {
+        // Logger variable
+        private readonly ILogger<PrivacyModel> _logger;
+
+        // Constructor that accepts logger as input
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// OnGet will essentially do nothing 
+        /// </summary>
+        public void OnGet()
+        {
+        }
+    }
+}
